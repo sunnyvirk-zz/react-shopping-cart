75e3eef7769cf14f42204281094386a60f735799
Sunny <sunny.virk@mac.com>
2015-02-25 21:57:41 +0000

    basic node setup, gulp config with jshint

---
+++ gulp/index.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import gulp from 'gulp';
+import util from 'gulp-util';
+import path from 'path';
+
+let dir = path.resolve(__dirname, 'tasks');
+for (let file of fs.readdirSync(dir)) {
+  let task = require(path.resolve(dir, file));
+  if(task.default) {
+    task = task.default;
+  }
+  if(typeof task !== 'function') {
+    util.log(`task ${file} must export a default function`);
+  }
+  task(gulp);
+}
